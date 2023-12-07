3d12d6b90bd755e134ce70f33e19eec7f1195b8e
7incho <martin30990@gmail.com>
2023-12-06 21:42:56 -0300

    ViewTransitions + Svelte components

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const curretTime = new Date()
+    const currentHour = curretTime.getHours()
+
+    let greetig = ''
+
+    if (currentHour<12){
+        greetig = 'Buenos Días'
+    } else if (currentHour < 18){
+        greetig = 'Buenas Tardes'
+    } else {
+        greetig = 'Buenas Noches'
+    }
+</script>
+
+<h1 class="text-4xl font-bold ">{greetig}</h1>
