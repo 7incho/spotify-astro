ff69eed05152b2afd28cbaa233f5634f2f848b76
7incho <martin30990@gmail.com>
2024-01-04 23:49:53 -0300

    Cambio de icono respecto al volumen

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,5 @@
 import { usePlayerStore } from "@/store/playerStore";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Slider } from "./Slider";
 
 export const Pause = () => (
@@ -14,32 +14,71 @@ export const Play = () => (
   </svg>
 );
 
-export const VolumeSilence = () => {
+export const VolumeSilence = () => (
   <svg
-    data-encore-id="icon"
+    fill="currentColor"
     role="presentation"
+    width="16"
+    height="16"
     aria-label="Volumen apagado"
     aria-hidden="true"
     viewBox="0 0 16 16"
-    class="Svg-sc-ytk21e-0 dAOlPY"
   >
     <path d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06z"></path>
     <path d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.642 3.642 0 0 0-1.33 4.967 3.639 3.639 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.73 4.73 0 0 1-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path>
-  </svg>;
-};
+  </svg>
+);
 
-export const Volume = () => {
+export const Volume = () => (
   <svg
-    data-encore-id="icon"
+    fill="currentColor"
     role="presentation"
-    aria-label="Volumen apagado"
+    width="16"
+    height="16"
+    aria-label="Volumen medio"
     aria-hidden="true"
     viewBox="0 0 16 16"
-    class="Svg-sc-ytk21e-0 dAOlPY"
   >
-    <path d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06z"></path>
-    <path d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.642 3.642 0 0 0-1.33 4.967 3.639 3.639 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.73 4.73 0 0 1-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path>
-  </svg>;
+    <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 6.087a4.502 4.502 0 0 0 0-8.474v1.65a2.999 2.999 0 0 1 0 5.175v1.649z"></path>
+  </svg>
+);
+
+export const VolumeControl = () => {
+  const volume = usePlayerStore((state) => state.volume);
+  const setVolume = usePlayerStore((state) => state.setVolume);
+  const previousVolumeRef = useRef(volume);
+
+  const isVolumeSilenced = volume < 0.1;
+
+  const handleClickVolume = () => {
+    if (isVolumeSilenced) {
+      setVolume(previousVolumeRef.current);
+    } else {
+      previousVolumeRef.current = volume;
+      setVolume(0);
+    }
+  };
+
+  return (
+    <div className="flex justify-center gap-x-2 text-white">
+      <button onClick={handleClickVolume}>
+        {isVolumeSilenced ? <VolumeSilence /> : <Volume />}
+      </button>
+
+      <Slider
+        defaultValue={[100]}
+        max={100}
+        min={0}
+        className="w-[95px]"
+        value={[volume * 100]}
+        onValueChange={(value) => {
+          const [newVolume] = value;
+          const volumeValue = newVolume / 100;
+          setVolume(volumeValue);
+        }}
+      />
+    </div>
+  );
 };
 
 const CurrentSong = ({ image, title, artists }) => {
@@ -57,7 +96,7 @@ const CurrentSong = ({ image, title, artists }) => {
 };
 
 export function Player() {
-  const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(
+  const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(
     (state) => state
   );
   const audioRef = useRef();
@@ -68,11 +107,15 @@ export function Player() {
   }, [isPlaying]);
 
   useEffect(() => {
+    audioRef.current.volume = volume;
+  }, [volume]);
+
+  useEffect(() => {
     const { song, playlist, songs } = currentMusic;
     if (song) {
       const src = `/music/${playlist?.id}/0${song.id}.mp3`;
       audioRef.current.src = src;
-      audioRef.current.volume = volumeRef.current;
+      audioRef.current.volume = volume;
       audioRef.current.play();
     }
   }, [currentMusic]);
@@ -99,18 +142,7 @@ export function Player() {
       <audio ref={audioRef} />
 
       <div className="grid place-content-center">
-        <Slider
-          defaultValue={[100]}
-          max={100}
-          min={0}
-          className="w-[95px]"
-          onValueChange={(value) => {
-            const [newVolume] = value;
-            const volumeValue = newVolume / 100;
-            volumeRef.current = volumeValue;
-            audioRef.current.volume = volumeValue;
-          }}
-        />
+        <VolumeControl />
       </div>
     </div>
   );
