ae2c4172c78c5b211f157294a44a505fc7062dd6
7incho <martin30990@gmail.com>
2024-01-19 21:34:02 -0300

    fix: deploy issues

--- .vercel/output/static/_astro/CardPlayButton.KWWXf66n.js
+++
@@ -1,1 +0,0 @@
-import{CardPlayButton as p}from"./CardPlayButton.ekmoL1CJ.js";import"./Player.xOpROyLf.js";import"./index.vPaQP66b.js";export{p as CardPlayButton};
--- .vercel/output/static/_astro/Player.edphVBfm.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./Player.xOpROyLf.js";import"./index.vPaQP66b.js";export{e as Player};
